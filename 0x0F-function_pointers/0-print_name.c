b6cc95cb13abea7aac1c4d8fa3c161b9297b1e86
Godsonic_Codes <antwigodson2525@gmail.com>
2023-09-16 00:07:51 -0700

    0-print_name.c

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,22 +1,17 @@
 #include "function_pointers.h"
-#include <stdlib.h>
+	#include <stdio.h>
+	/**
+	 * print_name - print name using pointer to function
+	 * @name: string to add
+	 * @f: pointer to function
+	 * Return: nothing
+	 **/
+	void print_name(char *name, void (*f)(char *))
+	{
+		if (name == NULL || f == NULL)
+			return;
 
-/**
-  * print_name - initializes a variable
-  *
-  * @name: name.
-  * @f: pointer.
-  * Return: nothing.
-  */
-void print_name(char *name, void (*f)(char *))
-{
-	if (name == NULL)
-	{
-		return;
+
+		f(name);
 	}
-	if (f == NULL)
-	{
-		return;
-	}
-	f(name);
-}
+
