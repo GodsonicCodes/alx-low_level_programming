867c067d7a753a6be2baf471017fa50fd1f59886
Godsonic_Codes <antwigodson2525@gmail.com>
2023-09-15 23:11:54 -0700

    0x0F-function_pointers

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,12 +1,12 @@
 #include "function_pointers.h"
 
 /**
-  * print_name - Prints a name
-  * @name: The name to prints
-  * @f: Pointer to function
-  *
-  * Return: Nothing
-  */
+ * print_name - prints a name
+ * @name: name to print
+ * @f: function to print with
+ *
+ * Return: void
+ */
 void print_name(char *name, void (*f)(char *))
 {
 	if (name != NULL && f != NULL)
--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -1,23 +1,22 @@
 #include "function_pointers.h"
 
 /**
-  * array_iterator - ...
-  * @array: ...
-  * @size: ...
-  * @action: ...
-  *
-  * Return: ...
-  */
+ * array_iterator - executes a func given as a param on each element of array
+ * @array: array to execute func on
+ * @size: size of array
+ * @action: pointer to the func to use
+ *
+ * Return: void
+ */
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
-	unsigned int i = 0;
+	size_t i;
 
-	if (array != NULL && action != NULL && size > 0)
+	if (array && action)
 	{
-		while (i < size)
+		for (i = 0; i < size; i++)
 		{
 			action(array[i]);
-			i++;
 		}
 	}
 }
