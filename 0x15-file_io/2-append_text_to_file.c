4d44bbd6fbbaa26a7eed46859a2efc1f254da88e
Godsonic_Codes <antwigodson2525@gmail.com>
2023-10-02 20:53:00 -0700

    0x15-file_io

---
+++ 0x15-file_io/2-append_text_to_file.c
@@ -0,0 +1,47 @@
+/*Append_text_to_file task,
+*Authur - Antwi Godson
+*/
+
+#include "main.h"
+
+
+
+/**
+ * append_text_to_file - this appends text at the end of a file.
+ * @filename: is the filename.
+ * @text_content: is the added content.
+ *
+ * Return: 1 if the file exists. -1 if it fails does not exist.
+ */
+
+
+/*append_text_to_file*/
+int append_text_to_file(const char *filename, char *text_content)
+{
+	int fd;
+	int nletters;
+	int rwr;
+
+	if (!filename)
+		return (-1);
+
+	fd = open(filename, O_WRONLY | O_APPEND);
+
+	if (fd == -1)
+		return (-1);
+
+	if (text_content)
+	{
+		for (nletters = 0; text_content[nletters]; nletters++)
+			;
+
+		rwr = write(fd, text_content, nletters);
+
+		if (rwr == -1)
+			return (-1);
+	}
+
+	close(fd);
+
+	return (1);
+}
